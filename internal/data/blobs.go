7ab95244526123e571be67f57d90a21bc03b44c1
Kiril R <kirilrknu@gmail.com>
2022-09-02 17:43:32 +0300

    Added BlobsQ interface, Blob struct

---
+++ internal/data/blobs.go
@@ -0,0 +1,20 @@
+package data
+
+type BlobsQ interface {
+	New() BlobsQ
+
+	Get() (*Blob, error)
+	Select() ([]Blob, error)
+	Update() ([]Blob, error)
+
+	Transaction(fn func(q BlobsQ) error) error
+
+	Insert(data Blob) (Blob, error)
+
+	FilterByID(id ...int64) BlobsQ
+}
+
+type Blob struct {
+	ID          int64  `db:"id" structs:"-"`
+	Information string `db:"information" structs:"information"`
+}
