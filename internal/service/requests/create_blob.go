4612bd81627e34a34943bce4a05323ce2546012a
Kiril R <kirilrknu@gmail.com>
2022-09-02 11:29:39 +0300

    Add CreateBlob request/handler basic structure

---
+++ internal/service/requests/create_blob.go
@@ -0,0 +1,40 @@
+package requests
+
+import (
+	"blob-svc/resources"
+	"encoding/json"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"net/http"
+)
+
+type CreateBlobRequest struct {
+	Data resources.CreateBlob
+}
+
+func NewCreateBlobRequest(r *http.Request) (CreateBlobRequest, error) {
+	var request CreateBlobRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return request, errors.Wrap(err, "failed to unmarshal")
+	}
+
+	return request, request.validate()
+}
+
+func (r *CreateBlobRequest) validate() error {
+	return mergeErrors(validation.Errors{
+		"/data/attributes/topic": validation.Validate(&r.Data.Attributes.Value, validation.Required,
+			validation.Length(3, 100)),
+	}).Filter()
+}
+
+func mergeErrors(validationErrors ...validation.Errors) validation.Errors {
+	result := make(validation.Errors)
+	for _, errs := range validationErrors {
+		for key, err := range errs {
+			result[key] = err
+		}
+	}
+	return result
+}
