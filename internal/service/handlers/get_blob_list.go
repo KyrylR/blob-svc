970fcac685bdd02089a02a39e13d77a1fc6f61e7
Kiril R <kirilrknu@gmail.com>
2022-09-04 13:36:14 +0300

    Deleted useless request parsing

--- internal/service/handlers/get_blob_list.go
+++ internal/service/handlers/get_blob_list.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"blob-svc/internal/data"
 	"blob-svc/internal/service/helpers"
-	"blob-svc/internal/service/requests"
 	"blob-svc/resources"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
@@ -11,13 +10,6 @@ import (
 )
 
 func GetBlobList(w http.ResponseWriter, r *http.Request) {
-	_, err := requests.NewGetBlobListRequest(r)
-	if err != nil {
-		helpers.Log(r).WithError(err).Info("wrong request")
-		ape.RenderErr(w, problems.BadRequest(err)...)
-		return
-	}
-
 	blobs, err := helpers.BlobsQ(r).Select()
 	if err != nil {
 		helpers.Log(r).WithError(err).Error("failed to get blobs")
