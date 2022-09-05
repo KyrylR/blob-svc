4333b85c413c5cbbb9970a39628a7119e5dfc1df
Kiril R <kirilrknu@gmail.com>
2022-09-05 16:22:42 +0300

    Regenerated files

---
+++ resources/model_blob_attributes.go
@@ -0,0 +1,12 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package resources
+
+import "encoding/json"
+
+type BlobAttributes struct {
+	// Information related to the blob
+	Information json.RawMessage `json:"information"`
+}
