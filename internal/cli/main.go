6e9cc5fffb189318363b6738f63421c5ec6a6864
Kiril R <kirilrknu@gmail.com>
2022-09-05 15:07:59 +0300

    Added migrate up to run service command (to make possible run service in one command)

--- internal/cli/main.go
+++ internal/cli/main.go
@@ -40,6 +40,7 @@ func Run(args []string) bool {
 
 	switch cmd {
 	case serviceCmd.FullCommand():
+		_ = MigrateUp(cfg)
 		service.Run(cfg)
 	case migrateUpCmd.FullCommand():
 		err = MigrateUp(cfg)
